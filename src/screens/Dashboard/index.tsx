1ee6489eef66a038c8faf50afc0b80e9e67f81cd
Michel Maia <dimermichel@gmail.com>
2021-09-11 17:08:09 -0300

    Hard Coded Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 import { 
     Container,
     Header,
@@ -9,9 +11,54 @@ import {
     User,
     UserGreeting,
     UserName,
+    Icon,
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList,
  } from './styles';
 
+ export interface DataListProps extends TransactionCardProps {
+     id: string;
+ }
+
 export function Dashboard() {
+    const data: DataListProps[] = [
+        { 
+            id: '1',
+            type: 'positive',
+            title: 'Web Dev',
+            amount: 'US$ 1,000.00',
+            category: {
+                name: 'Web Dev',
+                icon: 'dollar-sign',
+            },
+            date: '10/10/2021',
+        },
+        { 
+            id: '2',
+            type: 'negative',
+            title: '5 Guys',
+            amount: 'US$ 10.00',
+            category: {
+                name: 'Food',
+                icon: 'coffee',
+            },
+            date: '10/10/2021',
+        },
+        { 
+            id: '3',
+            type: 'negative',
+            title: 'Nike',
+            amount: 'US$ 100.00',
+            category: {
+                name: 'Clothes',
+                icon: 'shopping-bag',
+            },
+            date: '10/10/2021',
+        },
+    ];
+
     return (
         <Container>
             <Header>
@@ -19,12 +66,42 @@ export function Dashboard() {
                     <UserInfo>
                         <Photo source={{uri: 'https://avatars.githubusercontent.com/u/32581187?v=4'}}/>
                         <User>
-                            <UserGreeting>Olá,</UserGreeting>
+                            <UserGreeting>Hi,</UserGreeting>
                             <UserName>Michel</UserName>
                         </User>
                     </UserInfo>
+                    <Icon name="power" />
                 </UserWrapper>
             </Header>
+            <HighlightCards>
+                <HighlightCard
+                    type="up" 
+                    title="Income" 
+                    amount="US$ 10,000.00" 
+                    lastTransaction="Last transaction September 10th" 
+                />
+                <HighlightCard 
+                    type="down"
+                    title="Outcome" 
+                    amount="US$ 1,000.00" 
+                    lastTransaction="Last transaction September 10th" 
+                />
+                <HighlightCard
+                    type="total" 
+                    title="Total" 
+                    amount="US$ 9,000.00" 
+                    lastTransaction="Last transaction September 10th" 
+                />
+            </HighlightCards>
+            <Transactions>
+                <Title>List</Title>
+                <TransactionList 
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) =>  <TransactionCard data={item} />}
+                />
+               
+            </Transactions>
             <StatusBar style="auto" />
         </Container>
     )
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { DataListProps } from '.';
 
 export const Container = styled.View`
     flex: 1;
@@ -12,13 +16,17 @@ export const Header = styled.View`
     background-color: ${({ theme }) => theme.colors.primary};
 
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
     width: 100%;
     padding: 0 24px;
+    margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 export const UserInfo = styled.View`
@@ -47,3 +55,43 @@ export const UserName = styled.Text`
     font-family: ${({ theme }) => theme.fonts.bold};
     font-size: ${RFValue(18)}px;
 `;
+
+export const Icon = styled(Feather)`
+    color:  ${({ theme }) => theme.colors.secondary};
+    font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+    horizontal: true,
+    showsHorizontalScrollIndicator: false,
+    contentContainerStyle: { paddingHorizontal: 24 },
+})`
+    width: 100%;
+
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+    flex: 1;
+    padding: 0 24px;
+
+    margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+    color:  ${({ theme }) => theme.colors.text_dark};
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(18)}px;
+
+    margin-bottom: ${RFValue(16)}px;
+`;
+
+export const TransactionList = styled(
+    FlatList as new () => FlatList<DataListProps>
+    ).attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: {
+        paddingBottom: getBottomSpace(),
+    }
+})``;
